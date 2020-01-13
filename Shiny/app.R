4d552a406ee85cbb254efbb7a3c25591c1375c08
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-01-13 14:57:23 +0100

    Added basic Shiny elements

---
+++ Shiny/app.R
@@ -0,0 +1,13 @@
+library(shiny)
+
+# =========================== Define user interface ============================
+ui <- fluidPage(
+)
+
+# ============================ Define server logic =============================
+server <- function(input, output) {
+
+}
+
+# ================================ Run the app =================================
+shinyApp(ui, server)
