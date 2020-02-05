f71f78eae76d1b72ad3e6dd34289c616dc124a0b
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-02-05 17:43:43 +0100

    Changed values back to default

--- src/initialObjects.py
+++ src/initialObjects.py
@@ -1,8 +1,8 @@
 # This script contains the initialization objects for the trainer, as well as all constants used in the backend calculations.
 import numpy as np
 
-n_init = 10 #TODO: change back to default of 21 after testing
-n_update = 5 # TODO: change back to default (100) after testing
+n_init = 21 #TODO: change back to default (21) after testing
+n_update = 79 # TODO: change back to default (79) after testing
 n_tot = n_init + n_update
 n_opt = 5
 plotting = False
