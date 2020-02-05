67f87d1aca92e570b94f3876669438299dfa2092
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-02-05 11:00:49 +0100

    Improved documentation

--- Shiny/app.R
+++ Shiny/app.R
@@ -3,8 +3,8 @@ library(shiny)
 library(reticulate)
 
 # ============== Initialize Python and R constants and functions ===============
-source_python("../src/GPy_logit_link.py")
-source_python("../src/0_Initial_objects.py")
+source_python("../src/functions.py")
+source_python("../src/initialObjects.py")
 
 # =========================== Define user interface ============================
 ui <- fluidPage(
