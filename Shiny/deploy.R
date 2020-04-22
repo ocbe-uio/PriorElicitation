139552b719b9b2d01ce9aa7b6b867f39f4a87ea5
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-04-22 11:30:33 +0200

    Replaced preload.R with deploy.R

--- Shiny/deploy.R
+++ Shiny/deploy.R
@@ -1,4 +1,18 @@
 # Script to deploy the Shiny app to the web
-
 library(rsconnect)
-deployApp(appDir="Shiny/")
+library(reticulate)
+
+# Sys.setenv(RETICULATE_PYTHON = "~/.virtualenvs/pyenv/bin/python")
+
+# virtualenv_create(
+#     'pyenv', python="~/.virtualenvs/pyenv/bin/python"
+# )
+# use_python("~/.virtualenvs/pyenv/bin/python")
+
+deployApp(
+    appDir = "Shiny/",
+    appFileManifest = "Shiny/fileManifest.txt",
+    appName = "elicit",
+    appTitle = "Prior Elicitation",
+    python = "~/.virtualenvs/pyenv/bin/python"
+)
--- Shiny/preload.R
+++
@@ -1,6 +0,0 @@
-#Sys.setenv(RETICULATE_PYTHON = "~/.virtualenvs/pyenv/bin/python")
-
-#reticulate::virtualenv_create('pyenv', python="~/.virtualenvs/pyenv/bin/python")
-# use_python("~/.virtualenvs/pyenv/bin/python")
-
-rsconnect::deployApp(appFileManifest="fileManifest.txt", appName="elicit")
