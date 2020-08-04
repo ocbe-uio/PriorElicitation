57f95b66a741fa3d314f10df532f83c2a6f6c469
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-08-04 15:40:33 +0200

    Splitted app.R into ui and server scripts

--- Shiny/server.R
+++ Shiny/server.R
@@ -1,3 +1,5 @@
+# This script contains the backend (server) operations of the Shiny app
+
 library(reticulate)
 library(shiny)
 library(rdrop2)
@@ -20,68 +22,6 @@ source_python("initialObjects.py")
 source_python("functions-veri.py")
 source_python("functions-pari.py")
 if (debug) message("##### Running in debug mode #####")
-
-# Defining user interface ====================================================
-
-ui <- fluidPage(
-	titlePanel("Prior elicitation"),
-	sidebarLayout(
-		position = "left",
-		sidebarPanel(
-			conditionalPanel(
-				condition = "input.start_veri",
-				"Decision", br(),
-				actionButton(
-					inputId = "realistic",
-					label = "Number is realistic",
-					style = "background-color:#00BB00"
-				),
-				actionButton(
-					inputId = "unrealistic",
-					label = "Number is not realistic",
-					style = "background-color:#BB0000"
-				)
-			),
-			conditionalPanel(
-				condition = "input.start_pari",
-				"Decision", br(),
-				actionButton(
-					inputId = "choose_left",
-					label = "Left plot is more realistic"
-				),
-				actionButton(
-					inputId = "choose_right",
-					label = "Right plot is more realistic"
-				)
-			)
-		),
-		mainPanel(
-			tabsetPanel(
-				type = "tabs",
-				selected = "Pari-PRECIOUS",
-				tabPanel(
-					"Veri-PRECIOUS",
-					actionLink("start_veri", "Click here to start"), br(),
-					fluidRow(
-						column(3, h1("Number: ")),
-						column(2, h1(textOutput("ss")))
-					),
-					fluidRow(
-						column(2, h6("Round: ")),
-						column(3, h6(textOutput("i")))
-					),
-					h2(uiOutput("final_link"))
-				),
-				tabPanel(
-					"Pari-PRECIOUS",
-					actionLink("start_pari", "Click here to start"), br(),
-					plotOutput("barplot_left"),
-					plotOutput("barplot_right")
-				)
-			)
-		)
-	)
-)
 
 # Define server logic ========================================================
 
@@ -347,7 +287,3 @@ server <- function(input, output, session) {
 		stopApp()
 	})
 }
-
-# Run the app ================================================================
-
-shinyApp(ui, server)
