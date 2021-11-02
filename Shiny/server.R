6e0a281cf02a17e507ee76a91db10a0eaf1a1df2
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-11-02 10:25:39 +0100

    Locking Python version

--- Shiny/server.R
+++ Shiny/server.R
@@ -11,6 +11,7 @@ library(scatterplot3d)
 virtualenv_create(
 	envname  = "PriorElicitationPyEnv",
 	python   = "python3",
+	version  = "3.8",
 	packages = c("scipy", "wheel", "numpy", "GPy", "matplotlib", "python-dateutil")
 )
 use_virtualenv("PriorElicitationPyEnv", required = FALSE)
